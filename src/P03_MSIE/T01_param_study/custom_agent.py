1902da9c41a99200cfeec246402cb067765791c1
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-11-16 17:49:31 +0700

    Add run

--- src/P03_MSIE/T01_param_study/custom_agent.py
+++ src/P03_MSIE/T01_param_study/custom_agent.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class FixedParamAgent:
-    def __init__(self, F, CR, MG):
-        self.F = F
-        self.CR = CR
-        self.MG = MG
+    def __init__(self, name, F_sc, CR_sc, MG_sc):
+        self.model_name = name
+        self.F_sc = F_sc
+        self.CR_sc = CR_sc
+        self.MG_sc = MG_sc
 
     def predict(self, observation, deterministic=True):
-        return np.array([self.F, self.CR, self.MG]), None
+        return np.array([self.F_sc, self.CR_sc, self.MG_sc]), None
